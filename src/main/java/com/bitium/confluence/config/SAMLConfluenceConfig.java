7b4d88c64dd6bba6cd43fd7ebacef7817aac2e5a
Ramone Karodia <rkarodia@gmail.com>
2016-02-01 19:23:24 +0200

    Refactored core SAMLConfluenceConfig.java logic into SAMLConfig.java atlassian-saml common lib

--- src/main/java/com/bitium/confluence/config/SAMLConfluenceConfig.java
+++ src/main/java/com/bitium/confluence/config/SAMLConfluenceConfig.java
@@ -21,116 +21,11 @@
  */
 package com.bitium.confluence.config;
 
-import org.apache.commons.lang.StringUtils;
+import com.bitium.saml.config.SAMLConfig;
 
-import com.atlassian.sal.api.pluginsettings.PluginSettings;
-import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
-import com.bitium.saml.SAMLConfig;
-
-public class SAMLConfluenceConfig implements SAMLConfig {
-	
-	private PluginSettings pluginSettings;
-
-	private String defaultBaseURL;
-	
-	public static final String ENTITY_ID_SETTING = "saml2.entityId";
-	public static final String LOGIN_URL_SETTING = "saml2.loginUrl";
-	public static final String LOGOUT_URL_SETTING = "saml2.logoutUrl";
-	public static final String X509_CERTIFICATE_SETTING = "saml2.x509Certificate";
-	public static final String IDP_REQUIRED_SETTING = "saml2.idpRequired";
-	public static final String REDIRECT_URL_SETTING = "saml2.redirectUrl";
-	public static final String AUTO_CREATE_USER_SETTING = "saml2.autoCreateUser";
-
-	public void setPluginSettingsFactory(PluginSettingsFactory pluginSettingsFactory) {
-		this.pluginSettings = pluginSettingsFactory.createGlobalSettings();
-	}
-	
-	public void setLoginUrl(String loginUrl) {
-		pluginSettings.put(LOGIN_URL_SETTING, loginUrl);
-	}
-
-	public void setLogoutUrl(String logoutUrl) {
-		pluginSettings.put(LOGOUT_URL_SETTING, logoutUrl);		
-	}
-
-	public void setEntityId(String entityId) {
-		pluginSettings.put(ENTITY_ID_SETTING, entityId);		
-	}
-
-	public void setX509Certificate(String x509Certificate) {
-		pluginSettings.put(X509_CERTIFICATE_SETTING, x509Certificate);		
-	}
-
-	public void setIdpRequired(String idpRequired) {
-		pluginSettings.put(IDP_REQUIRED_SETTING, idpRequired);		
-	}
-	
-	public void setRedirectUrl(String redirectUrl) {
-		pluginSettings.put(REDIRECT_URL_SETTING, redirectUrl);		
-	}
-	
-	public String getIdpRequired() {
-		return StringUtils.defaultString((String)pluginSettings.get(IDP_REQUIRED_SETTING));
-	}
-	
-	public boolean getIdpRequiredFlag() {
-		if (StringUtils.defaultString((String)pluginSettings.get(IDP_REQUIRED_SETTING)).equals("true")) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public void setAutoCreateUser(String autoCreateUser) {
-		pluginSettings.put(AUTO_CREATE_USER_SETTING, autoCreateUser);
-	}
-
-	public String getAutoCreateUser() {
-		return StringUtils.defaultString((String)pluginSettings.get(AUTO_CREATE_USER_SETTING));
-	}
-
-	public boolean getAutoCreateUserFlag() {
-		if (StringUtils.defaultString((String)pluginSettings.get(AUTO_CREATE_USER_SETTING)).equals("true")) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public String getLoginUrl() {
-		return StringUtils.defaultString((String)pluginSettings.get(LOGIN_URL_SETTING));
-	}
-
-	public String getLogoutUrl() {
-		return StringUtils.defaultString((String)pluginSettings.get(LOGOUT_URL_SETTING));
-	}
-
-	public String getIdpEntityId() {
-		return StringUtils.defaultString((String)pluginSettings.get(ENTITY_ID_SETTING));
-	}
-
-	public String getX509Certificate() {
-		return StringUtils.defaultString((String)pluginSettings.get(X509_CERTIFICATE_SETTING));
-	}
-	
-	public String getRedirectUrl() {
-		return StringUtils.defaultString((String)pluginSettings.get(REDIRECT_URL_SETTING));
-	}
-
-	public void setDefaultBaseUrl(String defaultBaseURL) {
-		this.defaultBaseURL = defaultBaseURL;		
-	}
+public class SAMLConfluenceConfig extends SAMLConfig {
 	
 	public String getAlias() {
 		return "confluenceSAML";
 	}
-
-	public String getBaseUrl() {
-		return StringUtils.defaultString(defaultBaseURL);
-	}
-
-	public String getSpEntityId() {
-		return defaultBaseURL + "/" + getAlias();
-	}
-
 }
